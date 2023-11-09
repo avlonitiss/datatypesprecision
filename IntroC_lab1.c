794ff3106b119033c68a8465b3a25e7dbcb8518f
Internet of Spyrus <avlonitiss@yahoo.co.uk>
2023-11-09 11:16:34 +0200

    solution to explicit numbers.
    the solution is to declare in b variable, at least one of the numbers as float using decimal point.

--- IntroC_lab1.c
+++ IntroC_lab1.c
@@ -5,9 +5,9 @@
 main() { 
 printf("Hello World\n\n");
 float a=0.1;
-float b=1/10;
+float b=1/10.0;
 
-printf ("b =  %2.10f",b);
+printf ("b =  %2.10f \n",b);
 if (a==b){
 	
 	printf("a= %2.10f",a);
